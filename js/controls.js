c4dc81b36c1b9c8a58374049db72a67ffcfaef44
lucas <lucass.michels@gmail.com>
2023-07-27 10:16:34 -0300

    adding all code to repository

---
+++ js/controls.js
@@ -0,0 +1,48 @@
+export default function Controls({
+    buttonPause,
+    buttonPlay,
+    buttonSet,
+    buttonStop
+}) {
+
+    function play() {
+        buttonPlay.classList.add('hide')
+        buttonPause.classList.remove('hide')
+        
+        buttonSet.classList.add('hide')
+        buttonStop.classList.remove('hide')
+    }
+
+    function pause() {
+        buttonPause.classList.add('hide')
+        buttonPlay.classList.remove('hide')
+    }
+
+    function getMinutes() {
+        let newMinutes = prompt('Quantos minutos?')
+        if (!newMinutes) {
+            return false
+        }
+    
+        return newMinutes
+    }
+
+    function reset() {
+        buttonPlay.classList.remove('hide')
+        buttonPause.classList.add('hide')
+    
+        buttonSet.classList.remove('hide')
+        buttonStop.classList.add('hide')
+    }
+
+    return {
+        reset,
+        play,
+        pause,
+        getMinutes
+    }
+}
+
+
+
+
